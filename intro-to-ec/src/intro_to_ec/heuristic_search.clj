ff4853807d8fbc2dab174794b724ffc7e953c88c
Avery Koranda <koran023@morris.umn.edu>
2019-09-18 15:21:26 -0500

    Start restructuring search algorithm around using priority-map
    Still need to include a call to the heuristic in add-children to add nodes into the map with actual priorities.

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -1,10 +1,10 @@
 (ns intro-to-ec.heuristic-search
   (:require [clojure.set :as cset]
-            [clojure.data.priority-map]))
+            [clojure.data.priority-map :as pm]))
 
 (defn remove-previous-node
-  [new-node frontier visited]
-  (remove (cset/union (set frontier) (set visited)) new-node))
+  [new-nodes frontier visited]
+  (remove (cset/union (set frontier) (set visited)) new-nodes))
 
 (defn generate-path
   [came-from node]
@@ -13,24 +13,24 @@
     (conj (generate-path came-from (get came-from node)) node)))
 
 (defn search
-  [{:keys [get-next-node add-children]}
+  [{:keys [add-children]}
    {:keys [goal? make-children]}
    start-node max-calls]
-  (loop [frontier [start-node]
+  (loop [frontier (priority-map start-node 0)
          came-from {start-node :start-node}
          num-calls 0]
     (println num-calls ": " frontier)
     (println came-from)
-    (let [current-node (get-next-node frontier)]
+    (let [current-node (first frontier)]
       (cond
         (goal? current-node) (generate-path came-from current-node)
         (= num-calls max-calls) :max-calls-reached
         :else
-        (let [kids (remove-previous-node
+        (let [children (remove-previous-node
                     (make-children current-node) frontier (keys came-from))]
           (recur
            (add-children
-            kids
-            (rest frontier))
-           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
+            children  
+            (pop frontier))
+           (reduce (fn [cf child] (assoc cf child current-node)) came-from children)
            (inc num-calls)))))))
