de0dc1e762bb02dd4c9a9894ff9891abbbee4821
Avery Koranda <koran023@morris.umn.edu>
2019-09-23 14:42:17 -0500

    Add message for when no solutions are found
    It's better than a NullPointerException...

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -31,14 +31,16 @@
          num-calls 0]
     (println num-calls ": " frontier)
     (println came-from)
-    (let [current-node (key (first frontier))]
-      (cond
-        (goal? current-node) (generate-path came-from current-node)
-        (= num-calls max-calls) :max-calls-reached
-        :else
-        (let [children (remove-previous-node
-                    (make-children current-node) frontier (keys came-from))]
-          (recur
-            (add-children children frontier heuristic)
-            (add-to-came-from children current-node came-from)
-            (inc num-calls)))))))
+    (if (nil? (first frontier))
+      :no-solutions-found
+      (let [current-node (key (first frontier))]
+        (cond
+          (goal? current-node) (generate-path came-from current-node)
+          (= num-calls max-calls) :max-calls-reached
+          :else
+          (let [children (remove-previous-node
+                      (make-children current-node) frontier (keys came-from))]
+            (recur
+              (add-children children frontier heuristic)
+              (add-to-came-from children current-node came-from)
+              (inc num-calls))))))))
